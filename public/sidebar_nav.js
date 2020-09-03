63063836223af623eb09463b1ae5e098c49fb771
Kanta Yamaoka (山岡幹太) <49053649+kantasv@users.noreply.github.com>
2020-09-03 18:08:13 +0900

    added comments at sidebar_nav.js

--- public/sidebar_nav.js
+++ public/sidebar_nav.js
@@ -1,4 +1,17 @@
-//loads sidebar_nav content
+/*
+このJSファイルを読み込むと、MDLが適用されているページの
+ハンバーガーメニューを以下の通りに描画し、リンクも適用します
+--
+ダッシュボード
+グループプロフィール編集
+幹事プロフィール編集
+グループマッチ開始
+設定
+お問い合わせorバグの報告
+ヘルプ
+ログアウト
+--
+*/
 
 document.getElementById('sidebar_nav').innerHTML=
 '<a class="mdl-navigation__link" href="dashboard.html">ダッシュボード</a>'+
@@ -9,19 +22,6 @@ document.getElementById('sidebar_nav').innerHTML=
 '<a class="mdl-navigation__link" href="help.html">ヘルプ</a>'+ 
 '<a class="mdl-navigation__link" href="javascript:firebase.auth().signOut();setTimeout(changeLocationAfterSignOut,600);">ログアウト</a>'
 
-
 function changeLocationAfterSignOut(){
 	window.location.href='index.html'
 }
-/*
-ダッシュボード
-グループプロフィール編集
-幹事プロフィール編集
-グループマッチ開始
-
-
-設定
-お問い合わせorバグの報告
-ヘルプ
-ログアウト
-*/
